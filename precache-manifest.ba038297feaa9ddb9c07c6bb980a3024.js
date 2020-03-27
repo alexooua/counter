aaecc2c403fbb8ec98e16766e25d50114eda59cc
alex <alextotalk@gmail.com>
2020-03-27 21:27:41 +0200

    Updates

---
+++ precache-manifest.ba038297feaa9ddb9c07c6bb980a3024.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb46135e504a31ca0b8ac74950d2c9fe",
+    "url": "/counter/index.html"
+  },
+  {
+    "revision": "752b595975eb9238c506",
+    "url": "/counter/static/css/main.39163779.chunk.css"
+  },
+  {
+    "revision": "a77a1ee6d3a8d58ee3df",
+    "url": "/counter/static/js/2.f41b92c6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/counter/static/js/2.f41b92c6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "752b595975eb9238c506",
+    "url": "/counter/static/js/main.80d26b7f.chunk.js"
+  },
+  {
+    "revision": "890e0606e2c9e34496c7",
+    "url": "/counter/static/js/runtime-main.01fdcc42.js"
+  }
+]);
